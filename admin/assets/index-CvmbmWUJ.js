8ff117759fd89ce7245f16ed7a751e3897f00ddc
elveahuang <elvea.huang@gmail.com>
2024-05-05 07:27:12 +0000

    Deploying to gh-pages from @ elveahuang/platform-lite@1aae390a1b2924f046cfe06335932b2320f93b8b 🚀

---
+++ admin/assets/index-CvmbmWUJ.js
@@ -0,0 +1,1 @@
+import{j as t,L as o}from"./index-BLeGAbl1.js";const s=()=>t.jsx("div",{children:t.jsx(o,{to:"/about",children:"about"})});export{s as default};
--- admin/assets/index-Drzs7lZg.js
+++
@@ -1,1 +0,0 @@
-import{aL as a}from"./index-CZvQAsNg.js";const o=()=>a("/api/admin/dashboard"),n=()=>a("/api/admin/workbench");export{o as d,n as w};

18526c8137898a5a6dfcbe68129a85e2aff0465a
elveahuang <elvea.huang@gmail.com>
2024-05-25 10:40:52 +0000

    Deploying to gh-pages from @ elveahuang/me@3b68ceb5d8371071e8353552a330638dcb72b141 🚀

--- mobile/assets/about-DOJkWPno.js
+++
@@ -1,1 +0,0 @@
-import{j as s,N as t,L as o}from"./index-DSL_L_LM.js";const a=()=>s.jsxs("div",{children:[s.jsx(t,{children:"About"}),s.jsx(o,{to:"/about",children:"About"})]});export{a as default};

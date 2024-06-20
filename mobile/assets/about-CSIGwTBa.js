dfdf442a2bd4fea75bd64486a5776b83d6be2502
elveahuang <elvea.huang@gmail.com>
2024-06-20 11:52:51 +0000

    Deploying to gh-pages from @ elveahuang/me@c2c5271cad10ecdebc03057566a1d763e86d8388 🚀

--- mobile/assets/about-CSIGwTBa.js
+++
@@ -1,1 +0,0 @@
-import{G as s,P as t,U as o}from"./index-C3qCqree.js";const a=()=>s.jsxs("div",{children:[s.jsx(t,{children:"About"}),s.jsx(o,{to:"/about",children:"About"})]});export{a as default};
